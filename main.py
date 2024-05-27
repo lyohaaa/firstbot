e1293a64d7f97a38b683070a1000802e26626f07
Cybersport <Cybersport@26kadr.internal>
2024-05-27 13:38:02 +0300

    fourth

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 bot = Bot(token = TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton('Отправь фото кота')
+button2 = KeyboardButton('Перейти на следующую клавиатуру')
+keyboard.add(button1, button2)
 
 async def set_commands(bot: Bot):
     commands = [
@@ -11,14 +17,21 @@ async def set_commands(bot: Bot):
         types.BotCommand(command='/info', description='Информация о боте'),
         types.BotCommand(command='/authors', description='Создатели бота'),
         types.BotCommand(command='/support', description='Поддержка проекта')
-
     ]
 
     await bot.set_my_commands(commands)
 
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.reply('Привет, я эхо-бот')
+    await message.reply('Привет, я эхо-бот', reply_markup=keyboard)
+
+@dp.message_handler(lambda message: message.text == 'Отправь фото кота')
+async def button_1_click(message: types.Message):
+    await bot.send_photo(message.chat_id)
+
+@dp.message_handler(lambda message: message.text == 'Кнопка 2')
+async def button_2_click(message: types.Message):
+    await message.answer('Ты нажал кнопку 2')
 
 @dp.message_handler(commands= 'help')
 async def start(message: types.Message):
